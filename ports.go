0559a99e5b3ddb56fd385c53ac214ff9bc36f502
24online24 <chiuzbaianrares20@gmail.com>
2022-10-12 09:16:45 +0300

    Ports added to table

--- ports.go
+++ ports.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"strconv"
 
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
@@ -20,48 +23,50 @@ func main() {
 		panic(err)
 	}
 
-	// a := app.New()
-	// w := a.NewWindow("GoDocker Containers")
-	// w.Resize(fyne.NewSize(600, 500))
-	// var data = [][]string{{"CONTAINER ID", "IMAGE", "COMMAND", "CREATED", "STATUS", "PORTS", "NAMES"}}
+	a := app.New()
+	w := a.NewWindow("GoDocker Containers")
+	w.Resize(fyne.NewSize(600, 500))
+	var data = [][]string{{"CONTAINER ID", "IMAGE", "COMMAND", "CREATED", "STATUS", "PORTS", "NAMES"}}
 	for _, container := range containers {
-		// temp := make([]string, 0)
-		// temp = append(temp, container.ID[:10])
-		// temp = append(temp, container.Image)
-		// temp = append(temp, container.Command)
-		// temp = append(temp, strconv.Itoa(int(container.Created)))
-		// temp = append(temp, container.Status)
-		// temp = append(temp, container.Ports)
+		temp := make([]string, 0)
+		temp = append(temp, container.ID[:10])
+		temp = append(temp, container.Image)
+		temp = append(temp, container.Command)
+		temp = append(temp, strconv.Itoa(int(container.Created)))
+		temp = append(temp, container.Status)
 
-		// temp = append(temp, container.Names...)
-		// data = append(data, temp)
-		// fmt.Println(container.ID[:10] + " " + container.Image)
-		for _, port := range container.Ports {
+		portString := ""
+		for index, port := range container.Ports {
 			if len(port.IP) > 0 {
-				fmt.Print(port.IP + ":")
+				portString += port.IP + ":"
 			}
 			if port.PublicPort != 0 {
-				fmt.Print(strconv.Itoa(int(port.PublicPort)) + "->")
+				portString += strconv.Itoa(int(port.PublicPort)) + "->"
 			}
-			fmt.Print(strconv.Itoa(int(port.PrivatePort)))
-			fmt.Print("/" + port.Type)
-			fmt.Print("\t\t")
+			portString += strconv.Itoa(int(port.PrivatePort)) + "/" + port.Type
+			if index < len(container.Ports)-1 {
+				portString += ", "
+			}
 		}
-		// fmt.Print(container.Ports)
-		fmt.Println()
+		temp = append(temp, portString)
+
+		temp = append(temp, container.Names...)
+		data = append(data, temp)
+		fmt.Println(temp)
+
 	}
 
-	// list := widget.NewTable(
-	// 	func() (int, int) {
-	// 		return len(data), len(data[0])
-	// 	},
-	// 	func() fyne.CanvasObject {
-	// 		return widget.NewLabel("wide content")
-	// 	},
-	// 	func(i widget.TableCellID, o fyne.CanvasObject) {
-	// 		o.(*widget.Label).SetText(data[i.Row][i.Col])
-	// 	})
+	list := widget.NewTable(
+		func() (int, int) {
+			return len(data), len(data[0])
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("wide content")
+		},
+		func(i widget.TableCellID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(data[i.Row][i.Col])
+		})
 
-	// w.SetContent(list)
-	// w.ShowAndRun()
+	w.SetContent(list)
+	w.ShowAndRun()
 }
