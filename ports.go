24b0603a0031faef2e9dc458fe9a41e1ea73c556
24online24 <chiuzbaianrares20@gmail.com>
2022-10-12 19:51:27 +0300

    Prepared for button

--- ports.go
+++ ports.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 
 	"fyne.io/fyne/v2"
@@ -47,9 +48,9 @@ func main() {
 			portString += strconv.Itoa(int(port.PrivatePort)) + "/" + port.Type
 			if index < len(container.Ports)-1 {
 				portString += ", "
-				if index%2 == 1 {
-					portString += "\n"
-				}
+				// if index%2 == 1 {
+				// 	portString += "\n"
+				// }
 			}
 		}
 		temp = append(temp, portString)
@@ -74,17 +75,36 @@ func main() {
 
 	/*
 		For every column we check every row for the longest entry (string).
+		If there are multiple lines in an entry, the length of the first line is taken into account.
 		We then format the column according to the longest entry.
 	*/
 	for column := 0; column < len(data[0]); column++ {
+		// var lineLenMax int = 0
 		var maxCharLen int = 0
+		// var lines int = 0
 		for row := 0; row < len(data); row++ {
 			if len(data[row][column]) > maxCharLen {
 				maxCharLen = len(data[row][column])
+				// var lineLen int = 0
+				// var newLineIndex = strings.Index(data[row][column], "\n")
+				// if newLineIndex > 0 {
+				// 	lineLen = newLineIndex
+				// } else {
+				// 	lineLen = len(data[row][column])
+				// }
+				// if lineLen > lineLenMax {
+				// 	lineLenMax = lineLen
+				// }
 			}
 		}
 		runningContainersTable.SetColumnWidth(column, 50+float32(maxCharLen)*7)
 	}
 	w.SetContent(runningContainersTable)
+
+	button := widget.NewButton("Running containers", func() {
+		fmt.Println("tapped")
+		w.SetContent(runningContainersTable)
+	})
+	w.SetContent(button)
 	w.ShowAndRun()
 }
