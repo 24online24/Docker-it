f62ff53a4537e99cd46c73254880c1b1adc3811c
24online24 <chiuzbaianrares20@gmail.com>
2022-10-25 15:02:17 +0300

    Fixed double goroutine

--- start/start.go
+++ start/start.go
@@ -95,83 +95,81 @@ func isDockerStarted(chDockerStarted chan int) {
 }
 
 func showRunningContainers(chRunningContainers chan *widget.Table, cli *client.Client) {
-	go func() {
-		for {
-			var data [][]string = [][]string{{"CONTAINER ID", "IMAGE", "COMMAND", "CREATED", "STATUS", "PORTS", "NAMES", "TERMINAL"}}
+	for {
+		var data [][]string = [][]string{{"CONTAINER ID", "IMAGE", "COMMAND", "CREATED", "STATUS", "PORTS", "NAMES", "TERMINAL"}}
 
-			containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: true, Limit: 10})
-			if err == nil {
-				for _, container := range containers {
-					var portString string = ""
-					for index, port := range container.Ports {
-						if len(port.IP) > 0 {
-							portString += port.IP + ":"
-						}
-						if port.PublicPort != 0 {
-							portString += strconv.Itoa(int(port.PublicPort)) + "->"
-						}
-						portString += strconv.Itoa(int(port.PrivatePort)) + "/" + port.Type
-						if index < len(container.Ports)-1 {
-							portString += ", "
-						}
+		containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: true, Limit: 10})
+		if err == nil {
+			for _, container := range containers {
+				var portString string = ""
+				for index, port := range container.Ports {
+					if len(port.IP) > 0 {
+						portString += port.IP + ":"
 					}
-					data = append(data, []string{
-						container.ID[:10], container.Image, container.Command,
-						strconv.Itoa(int(time.Now().Unix()-container.Created)) + " seconds ago", container.Status,
-						portString, container.Names[0], "OPEN",
-					})
-				}
-			}
-			runningContainersTable := widget.NewTable(
-				func() (int, int) {
-					return len(data), len(data[0])
-				},
-				func() fyne.CanvasObject {
-					tab := widget.NewLabel("")
-					return tab
-				},
-				func(i widget.TableCellID, o fyne.CanvasObject) {
-					o.(*widget.Label).SetText(data[i.Row][i.Col])
-				},
-			)
-			for column := 0; column < len(data[0]); column++ {
-				var maxCharLen int = 0
-				for row := 0; row < len(data); row++ {
-					if len(data[row][column]) > maxCharLen {
-						maxCharLen = len(data[row][column])
+					if port.PublicPort != 0 {
+						portString += strconv.Itoa(int(port.PublicPort)) + "->"
+					}
+					portString += strconv.Itoa(int(port.PrivatePort)) + "/" + port.Type
+					if index < len(container.Ports)-1 {
+						portString += ", "
 					}
 				}
-				runningContainersTable.SetColumnWidth(column, 50+float32(maxCharLen)*7)
+				data = append(data, []string{
+					container.ID[:10], container.Image, container.Command,
+					strconv.Itoa(int(time.Now().Unix()-container.Created)) + " seconds ago", container.Status,
+					portString, container.Names[0], "OPEN",
+				})
 			}
-			runningContainersTable.OnSelected = func(i widget.TableCellID) {
-				if i.Col == 7 && i.Row > 0 {
-					var cmd *exec.Cmd
-					if runtime.GOOS == "windows" {
-						cmd = exec.Command("cmd", "/c", "start", "cmd", "/c", "docker", "exec", "-ti", data[i.Row][0], "/bin/bash")
-					} else if runtime.GOOS == "linux" {
-						testcmd := exec.Command("command", "-v", "gnome-terminal")
+		}
+		runningContainersTable := widget.NewTable(
+			func() (int, int) {
+				return len(data), len(data[0])
+			},
+			func() fyne.CanvasObject {
+				tab := widget.NewLabel("")
+				return tab
+			},
+			func(i widget.TableCellID, o fyne.CanvasObject) {
+				o.(*widget.Label).SetText(data[i.Row][i.Col])
+			},
+		)
+		for column := 0; column < len(data[0]); column++ {
+			var maxCharLen int = 0
+			for row := 0; row < len(data); row++ {
+				if len(data[row][column]) > maxCharLen {
+					maxCharLen = len(data[row][column])
+				}
+			}
+			runningContainersTable.SetColumnWidth(column, 50+float32(maxCharLen)*7)
+		}
+		runningContainersTable.OnSelected = func(i widget.TableCellID) {
+			if i.Col == 7 && i.Row > 0 {
+				var cmd *exec.Cmd
+				if runtime.GOOS == "windows" {
+					cmd = exec.Command("cmd", "/c", "start", "cmd", "/c", "docker", "exec", "-ti", data[i.Row][0], "/bin/bash")
+				} else if runtime.GOOS == "linux" {
+					testcmd := exec.Command("command", "-v", "gnome-terminal")
+					testerr := testcmd.Run()
+					if testerr == nil {
+						cmd = exec.Command("gnome-terminal", "-e", "docker", "exec", "-ti", data[i.Row][0], "/bin/bash")
+					} else {
+						testcmd := exec.Command("command", "-v", "konsole")
 						testerr := testcmd.Run()
 						if testerr == nil {
-							cmd = exec.Command("gnome-terminal", "-e", "docker", "exec", "-ti", data[i.Row][0], "/bin/bash")
-						} else {
-							testcmd := exec.Command("command", "-v", "konsole")
-							testerr := testcmd.Run()
-							if testerr == nil {
-								cmd = exec.Command("konsole", "-e", "docker", "exec", "-ti", data[i.Row][0], "/bin/bash")
-							}
+							cmd = exec.Command("konsole", "-e", "docker", "exec", "-ti", data[i.Row][0], "/bin/bash")
 						}
 					}
-					err := cmd.Run()
-					if err != nil {
-						log.Fatal(err)
-					}
 				}
-				runningContainersTable.UnselectAll()
+				err := cmd.Run()
+				if err != nil {
+					log.Fatal(err)
+				}
 			}
-			chRunningContainers <- runningContainersTable
-			time.Sleep(time.Second)
+			runningContainersTable.UnselectAll()
 		}
-	}()
+		chRunningContainers <- runningContainersTable
+		time.Sleep(time.Second)
+	}
 }
 
 func main() {
