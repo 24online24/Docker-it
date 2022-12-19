dd1654fa550ee2e68e5b50c55b25a90797589d8a
24online24 <chiuzbaianrares20@gmail.com>
2022-12-19 16:19:24 +0200

    Compose forms

--- dev/tabs.go
+++ dev/tabs.go
@@ -82,14 +82,12 @@ func createComposeTab(cli *client.Client) *fyne.Container {
 
 	form := container.NewGridWithColumns(3)
 
-	container_compose := container.NewHBox(
+	container_compose := container.NewVBox(
 		container.NewVBox(
 			layout.NewSpacer(),
-			container.New(layout.NewHBoxLayout(),
-				layout.NewSpacer(),
+			container.NewHBox(
 				widget.NewLabel("Number of services/ containers:"),
 				nrOfServicesEntry,
-				layout.NewSpacer(),
 				widget.NewButton("Create form", func() {
 					nrOfServices, err := strconv.Atoi(nrOfServicesEntry.Text)
 
@@ -97,60 +95,20 @@ func createComposeTab(cli *client.Client) *fyne.Container {
 						log.Fatal(err)
 					}
 					for i := 1; i <= nrOfServices; i++ {
-						form.Add(container.NewVBox(widget.NewLabel("Test")))
+						form.Add(container.NewVBox(
+							container.NewHBox(
+								widget.NewLabel(""),
+							),
+						))
 					}
 				}),
 			),
-			form,
-			// container.New(layout.NewGridLayoutWithColumns(4),
-			// 	layout.NewSpacer(),
-			// 	widget.NewLabel("Select your favourite theme:"),
-			// 	theme_select,
-			// 	layout.NewSpacer(),
-			// ),
-			// container.New(layout.NewGridLayoutWithColumns(4),
-			// 	layout.NewSpacer(),
-			// 	widget.NewLabel("Terminal path to executable:"),
-			// 	terminal,
-			// 	layout.NewSpacer(),
-			// ),
-			// container.New(layout.NewGridLayoutWithColumns(4),
-			// 	layout.NewSpacer(),
-			// 	widget.NewLabel("Docker Desktop.exe path (Windows only):"),
-			// 	docker_e,
-			// 	layout.NewSpacer(),
-			// ),
+			layout.NewSpacer(),
 			// layout.NewSpacer(),
-			// container.NewHBox(
-			// 	layout.NewSpacer(),
-			// 	layout.NewSpacer(),
-			// 	layout.NewSpacer(),
-			// 	widget.NewButtonWithIcon("Save", theme.DocumentSaveIcon(), func() {
-			// 		// TODO add theme here aswell
-			// 		refresh_rate, _ = strconv.Atoi(rrate.Text)
-			// 		terminal_setting = terminal.Text
-			// 		if env == "windows" {
-			// 			docker_path = docker_e.Text
-			// 		}
-			// 		save_settings()
-			// 	}),
-			// 	layout.NewSpacer(),
-			// 	widget.NewButtonWithIcon("Cancel", theme.CancelIcon(), func() {
-			// 		get_settings()
-			// 		rrate.Text = fmt.Sprint(refresh_rate)
-			// 		terminal.Text = terminal_setting
-			// 		if env == "windows" {
-			// 			docker_e.Text = docker_path
-			// 			docker_e.Refresh()
-			// 		}
-			// 		rrate.Refresh()
-			// 		terminal.Refresh()
-
-			// 	}),
-			// 	layout.NewSpacer(),
-			// 	layout.NewSpacer(),
-			// 	layout.NewSpacer(),
-			// ),
+			form,
+			// layout.NewSpacer(),
+			// layout.NewSpacer(),
+			// layout.NewSpacer(),
 			layout.NewSpacer(),
 		))
 
